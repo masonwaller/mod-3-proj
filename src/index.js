18c0be877b96646a30462c84dd84f475c0265122
Heidi Boulter <boulter.heidi@gmail.com>
2019-12-30 13:16:33 -0600

    styling edits

--- src/index.js
+++ src/index.js
@@ -2,72 +2,70 @@
 // .then(res => res.json())
 // .then(res => displayUsers(res))
 
-window.addEventListener("DOMContentLoaded", (e) => {
-    const login = document.getElementById("name");
-    let arr;
-    let user;
+window.addEventListener("DOMContentLoaded", e => {
+  const login = document.getElementById("name");
+  let arr;
+  let user;
 
-    fetch("http://127.0.0.1:3000/api/v1/users")
+  fetch("http://127.0.0.1:3000/api/v1/users")
     .then(res => res.json())
     .then(res => {
-        arr = res;
-    })
-    
-    login.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let nameInput = document.getElementById("name_input").value; 
-        user = arr.find(user => nameInput == user.name)
-        
-        if (user) {
-            console.log(user)
-        }
-        else {
-            fetch(`http://127.0.0.1:3000/api/v1/users`, {
-                method: "POST",
-                headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({name: nameInput})
-            }).then(fetch("http://127.0.0.1:3000/api/v1/users").then(res => res.json()).then(res => {user = res[res.length -1]}))
-        }
-        displayInstructions()
-    })
+      arr = res;
+    });
 
-    function displayInstructions() {
-        let title = document.getElementById("title")
-        let instructions = document.getElementById("instructions")
-        let butt = document.createElement("button")
+  login.addEventListener("submit", event => {
+    event.preventDefault();
+    let nameInput = document.getElementById("name_input").value;
+    user = arr.find(user => nameInput == user.name);
 
-        butt.id = "button"
-        butt.innerHTML = "Next ->"
-        login.remove()
-        title.innerHTML = "Instructions:"
-        instructions.innerHTML = "You will have around 10-15 seconds of a movie clip played. <br> Your job is to click the answer of the movie title.  <br> Your score will be tallied up at the end.<br><br>"
-        instructions.appendChild(butt)
-        
-        butt.addEventListener("click", (e) => {
-            e.preventDefault()
-            displayMovies()
-            console.log(user)
-        })
+    if (user) {
+      console.log(user);
+    } else {
+      fetch(`http://127.0.0.1:3000/api/v1/users`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: nameInput })
+      }).then(
+        fetch("http://127.0.0.1:3000/api/v1/users")
+          .then(res => res.json())
+          .then(res => {
+            user = res[res.length - 1];
+          })
+      );
     }
+    displayInstructions();
+  });
 
-    let videos;
-    fetch("http://127.0.0.1:3000/api/v1/videos")
+  function displayInstructions() {
+    let title = document.getElementById("title");
+    let instructions = document.getElementById("instructions");
+    let butt = document.createElement("button");
+
+    butt.id = "button";
+    butt.innerHTML = "Next";
+    login.remove();
+    title.innerHTML = "Instructions:";
+    instructions.innerHTML =
+      "You will have around 10-15 seconds of a movie clip played. <br> Your job is to click the answer of the movie title.  <br> Your score will be tallied up at the end.<br><br><br>";
+    instructions.appendChild(butt);
+
+    butt.addEventListener("click", e => {
+      e.preventDefault();
+      displayMovies();
+      console.log(user);
+    });
+  }
+
+  let videos;
+  fetch("http://127.0.0.1:3000/api/v1/videos")
     .then(res => res.json())
     .then(res => {
-        videos = res;
-        console.log(videos)
-    })
+      videos = res;
+      console.log(videos);
+    });
 
-    function displayMovies() {
-        
-    }
-
-
-
-
-
-
-})
+  function displayMovies() {}
+});
