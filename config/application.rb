37ea1bfa09e69634e0ffef9ea5f346990d2e8984
github username <github email address>
2020-01-27 10:47:17 -0600

    Heroku problems

--- config/application.rb
+++ config/application.rb
@@ -36,9 +36,3 @@ module Mod3Project
   end
 end
 
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
-   end
-end
