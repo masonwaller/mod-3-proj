31a50375d456acbaaca764bc825c7c3d0e91405d
github username <github email address>
2020-01-27 09:29:56 -0600

    Heroku fetches

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", e => {
     let user;
     let score = 0;
     let unique;
-    fetch("http://127.0.0.1:3000/api/v1/users")
+    fetch("http://agile-reaches-30710.heroku.com/api/v1/users")
       .then(res => res.json())
       .then(res => {
         arr = res;
@@ -38,7 +38,7 @@ window.addEventListener("DOMContentLoaded", e => {
       if (user) {
         console.log(user.id);
       } else {
-        fetch(`http://127.0.0.1:3000/api/v1/users`, {
+        fetch(`http://agile-reaches-30710.heroku.com/api/v1/users`, {
           method: "POST",
           headers: {
             Accept: "application/json",
@@ -46,7 +46,7 @@ window.addEventListener("DOMContentLoaded", e => {
           },
           body: JSON.stringify({ name: nameInput })
         }).then(() =>
-          fetch("http://127.0.0.1:3000/api/v1/users")
+          fetch("http://agile-reaches-30710.heroku.com/api/v1/users")
             .then(res => res.json())
             .then(res => {
               user = res[res.length - 1];
@@ -72,7 +72,7 @@ window.addEventListener("DOMContentLoaded", e => {
       });
     }
     let videos;
-    fetch("http://127.0.0.1:3000/api/v1/videos")
+    fetch("http://agile-reaches-30710.heroku.com/api/v1/videos")
       .then(res => res.json())
       .then(res => {
         videos = res;
@@ -158,7 +158,7 @@ window.addEventListener("DOMContentLoaded", e => {
       }
     }
     function displayScore() {
-      fetch(`http://127.0.0.1:3000/api/v1/scores`, {
+      fetch(`http://agile-reaches-30710.heroku.com/api/v1/scores`, {
         method: "POST",
         headers: {
           Accept: "application/json",
@@ -203,7 +203,7 @@ window.addEventListener("DOMContentLoaded", e => {
       menu();
     }
     async function showAll() {
-      let scoreArr = await fetch("http://127.0.0.1:3000/api/v1/scores");
+      let scoreArr = await fetch("http://agile-reaches-30710.heroku.com/api/v1/scores");
       let result = await scoreArr.json();
       let potato = result.sort(function(a, b) {
         return b.score - a.score;
